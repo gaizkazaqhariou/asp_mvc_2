5b1e8ac1e61dbb6b38972f955932c5def28d1e45
gaizkazaqhariou <muhammad_zaqhariou_24rpl@student.smktelkom-mlg.sch.id>
2017-03-07 06:31:50 +0700

    final modul 1

--- asp_mvc_2/App_Start/RouteConfig.cs
+++ asp_mvc_2/App_Start/RouteConfig.cs
@@ -16,8 +16,14 @@ namespace asp_mvc_2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
         }
+
     }
 }
